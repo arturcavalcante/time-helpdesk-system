d21ce661e973ff7eca03de1b76981a1aa2b18dba
arturcavalcante <deliraschaupenlehnercavalcante@gmail.com>
2026-04-16 14:55:04 -0300

    Base do sistema copiada e ajustada pois perdi parte do projeto

---
+++ TimeHelpDesk/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace TimeHelpDesk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.lstChamados = new System.Windows.Forms.ListBox();
+            this.btnResolver = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuário";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(32, 41);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(155, 20);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Descrição do chamado";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(32, 80);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(155, 20);
+            this.txtDescricao.TabIndex = 3;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(32, 106);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 4;
+            this.btnAbrir.Text = "Abrir chamado";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // lstChamados
+            // 
+            this.lstChamados.FormattingEnabled = true;
+            this.lstChamados.Location = new System.Drawing.Point(207, 25);
+            this.lstChamados.Name = "lstChamados";
+            this.lstChamados.Size = new System.Drawing.Size(486, 108);
+            this.lstChamados.TabIndex = 5;
+            // 
+            // btnResolver
+            // 
+            this.btnResolver.Location = new System.Drawing.Point(207, 139);
+            this.btnResolver.Name = "btnResolver";
+            this.btnResolver.Size = new System.Drawing.Size(75, 23);
+            this.btnResolver.TabIndex = 6;
+            this.btnResolver.Text = "Resolver chamado";
+            this.btnResolver.UseVisualStyleBackColor = true;
+            this.btnResolver.Click += new System.EventHandler(this.btnResolver_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnResolver);
+            this.Controls.Add(this.lstChamados);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.ListBox lstChamados;
+        private System.Windows.Forms.Button btnResolver;
+    }
+}
+
---
+++ TimeHelpDesk/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeHelpDesk
+{
+    public partial class Form1 : Form
+    {
+        List<string> chamados = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            // garante foco inicial (opcional manter comentado se quiser)
+            // this.BeginInvoke(new Action(() => txtUsuario.Focus()));
+
+            CarregarChamados();
+        }
+        private void CarregarChamados()
+        {
+            if (System.IO.File.Exists("chamados.txt"))
+            {
+                chamados = System.IO.File.ReadAllLines("chamados.txt")
+                    .Where(c => !string.IsNullOrWhiteSpace(c))
+                    .ToList();
+
+                foreach (var chamado in chamados)
+                {
+                    lstChamados.Items.Add(chamado);
+                }
+            }
+        }
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUsuario.Text) ||
+                string.IsNullOrWhiteSpace(txtDescricao.Text))
+            {
+                MessageBox.Show("Preencha usuário e descrição.");
+                return;
+            }
+
+            string usuario = txtUsuario.Text;
+            string descricao = txtDescricao.Text;
+
+            string chamadoFormatado =
+                "[ABERTO] [" + DateTime.Now.ToString("dd/MM HH:mm") + "] " +
+                usuario + " - " + descricao;
+
+            chamados.Add(chamadoFormatado);
+            lstChamados.Items.Add(chamadoFormatado);
+
+            SalvarChamados();
+
+            txtDescricao.Clear();
+            txtUsuario.Clear();
+        }
+        private void btnResolver_Click(object sender, EventArgs e)
+        {
+            if (lstChamados.SelectedItem == null)
+            {
+                MessageBox.Show("Selecione um chamado.");
+                return;
+            }
+
+            string chamadoSelecionado = lstChamados.SelectedItem.ToString();
+
+            string chamadoAtualizado;
+
+            if (chamadoSelecionado.Contains("[ABERTO]"))
+            {
+                chamadoAtualizado = chamadoSelecionado.Replace("[ABERTO]", "[RESOLVIDO]");
+            }
+            else if (!chamadoSelecionado.Contains("[RESOLVIDO]"))
+            {
+                chamadoAtualizado = "[RESOLVIDO] " + chamadoSelecionado;
+            }
+            else
+            {
+                MessageBox.Show("Chamado já está resolvido.");
+                return;
+            }
+
+            int index = lstChamados.SelectedIndex;
+
+            lstChamados.Items[index] = chamadoAtualizado;
+            chamados[index] = chamadoAtualizado;
+
+            SalvarChamados();
+        }
+        void SalvarChamados()
+        {
+            System.IO.File.WriteAllLines("chamados.txt", chamados);
+        }
+    }
+}
